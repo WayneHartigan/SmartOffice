047942ead8d4ad075a92dad4032ef1a743ec12a4
Wayne Hartigan <hartiganwayne@gmail.com>
2020-03-30 21:43:05 +0100

    Working light power service, client and server

--- src/main/java/light/LightClient.java
+++ src/main/java/light/LightClient.java
@@ -1,5 +1,35 @@
 package light;
 
-public class LightClient {
+import java.util.logging.Logger;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class LightClient {
+	private static  Logger logger = Logger.getLogger(LightClient.class.getName());
+
+	private static LightServiceGrpc.LightServiceBlockingStub blockingStub;
+	private static LightServiceGrpc.LightServiceStub asyncStub;
+	private static LightServiceGrpc.LightServiceFutureStub futureStub;
+
+	public static void main(String[] args) throws Exception {
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+
+		blockingStub = LightServiceGrpc.newBlockingStub(channel);
+		asyncStub = LightServiceGrpc.newStub(channel);
+		futureStub =  LightServiceGrpc.newFutureStub(channel);
+
+		switchPower();
+
+	}
+	
+	public static void switchPower() {
+
+		PowerRequest request = PowerRequest.newBuilder().setSwitch(false).build();
+
+		PowerResponse response = blockingStub.switchPower(request);
+
+        System.out.println(response.getSwitch());
+
+    }
 }
--- src/main/java/light/LightServer.java
+++ src/main/java/light/LightServer.java
@@ -1,5 +1,43 @@
 package light;
+import java.io.IOException;
+import java.util.logging.Logger;
 
-public class LightServer {
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import light.PowerRequest;
+import light.PowerResponse;
+import light.LightServiceGrpc.LightServiceImplBase;
+import io.grpc.stub.StreamObserver;
 
+public class LightServer extends LightServiceImplBase{
+	
+	private static final Logger logger = Logger.getLogger(LightServer.class.getName());
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		LightServer lightserver = new LightServer();
+	   
+	    int port = 50051;
+	    Server server = ServerBuilder.forPort(port)
+	        .addService(lightserver)
+	        .build()
+	        .start();
+	    
+	    logger.info("Server started, listening on " + port);
+	    		    
+	    server.awaitTermination();
+ }
+	
+	public void switchPower(PowerRequest request, StreamObserver<PowerResponse> responseObserver) {
+        System.out.println("Receiving request...");
+
+        System.out.println("Light power");
+
+        Boolean power = request.getSwitch(); 
+        
+        PowerResponse response = PowerResponse.newBuilder().setSwitch(power).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+  }
 }
