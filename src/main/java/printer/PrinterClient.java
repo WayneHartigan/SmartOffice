c6953d806089bed81d91e8b15c58605b503747b6
Wayne Hartigan <hartiganwayne@gmail.com>
2020-04-01 19:01:49 +0100

    Added power switch for printer

--- src/main/java/printer/PrinterClient.java
+++ src/main/java/printer/PrinterClient.java
@@ -1,5 +1,38 @@
 package printer;
 
+import java.util.logging.Logger;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import printer.PrinterClient;
+
 public class PrinterClient {
+	
+	private static  Logger logger = Logger.getLogger(PrinterClient.class.getName());
+	
+	private static PrinterServiceGrpc.PrinterServiceBlockingStub blockingStub;
+	private static PrinterServiceGrpc.PrinterServiceStub asyncStub;
+	private static PrinterServiceGrpc.PrinterServiceFutureStub futureStub;
+	
+	public static void main(String[] args) throws Exception {
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+
+		blockingStub = PrinterServiceGrpc.newBlockingStub(channel);
+		asyncStub = PrinterServiceGrpc.newStub(channel);
+		futureStub =  PrinterServiceGrpc.newFutureStub(channel);
+
+		switchPower();
+
+	}
+	
+	public static void switchPower() {
+
+		PowerRequest request = PowerRequest.newBuilder().setSwitch(false).build();
+
+		PowerResponse response = blockingStub.switchPower(request);
+
+        System.out.println(response.getSwitch());
+
+    }
 
 }
--- src/main/java/printer/PrinterServer.java
+++ src/main/java/printer/PrinterServer.java
@@ -1,5 +1,45 @@
 package printer;
 
-public class PrinterServer {
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import printer.PowerRequest;
+import printer.PowerResponse;
+import printer.PrinterServiceGrpc.PrinterServiceImplBase;
+
+public class PrinterServer extends PrinterServiceImplBase{
+	
+	private static final Logger logger = Logger.getLogger(PrinterServer.class.getName());
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		PrinterServer printerserver = new PrinterServer();
+	   
+		int port = 50051;
+	    Server server = ServerBuilder.forPort(port)
+	        .addService(printerserver)
+	        .build()
+	        .start();
+	    
+	    logger.info("Server started, listening on " + port);
+	    		    
+	    server.awaitTermination();
+	}
+	
+	public void switchPower(PowerRequest request, StreamObserver<PowerResponse> responseObserver) {
+        System.out.println("Receiving request...");
+
+        System.out.println("Light power");
+
+        Boolean power = request.getSwitch(); 
+        
+        PowerResponse response = PowerResponse.newBuilder().setSwitch(power).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+	}
 
 }
