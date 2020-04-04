6461c18f218be57afba3a2b33671b5afccfacb91
Wayne Hartigan <hartiganwayne@gmail.com>
2020-04-04 19:20:41 +0100

    Updated register and discovery for jmdns

--- SmartOffice/src/main/java/jmdnsfiles/SmartOfficeServiceDiscovery.java
+++ SmartOffice/src/main/java/jmdnsfiles/SmartOfficeServiceDiscovery.java
@@ -6,42 +6,42 @@ import java.net.UnknownHostException;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceEvent;
-import javax.jmdns.ServiceInfo;
 import javax.jmdns.ServiceListener;
 
 public class SmartOfficeServiceDiscovery {
 	
 	private static class SampleListener implements ServiceListener {
-		@Override
-		public void serviceAdded(ServiceEvent event) {
-			System.out.println("Service added: " + event.getInfo());
-		}
+        @Override
+        public void serviceAdded(ServiceEvent event) {
+            System.out.println("Service added: " + event.getInfo());
+        }
 
-		@Override
-		public void serviceRemoved(ServiceEvent event) {
-			System.out.println("Service removed: " + event.getInfo());
-		}
+        @Override
+        public void serviceRemoved(ServiceEvent event) {
+            System.out.println("Service removed: " + event.getInfo());
+        }
 
-		@Override
-		public void serviceResolved(ServiceEvent event) {
-                    ServiceInfo info = event.getInfo();
-                    int port = info.getPort();
-                    String path = info.getNiceTextString().split("=")[1];
-                    GetRequest.request("http://localhost:"+port+"/"+path);
-		}
-	}
+        @Override
+        public void serviceResolved(ServiceEvent event) {
+            System.out.println("Service resolved: " + event.getInfo());
+        }
+    }
 
-	public static void main(String[] args) throws InterruptedException {
-		try {
-			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            // Create a JmDNS instance
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
 
-			jmdns.addServiceListener("_http._tcp.local.", new SampleListener());
+            // Add a service listener
+            jmdns.addServiceListener("_http._tcp.local.", new SampleListener());
 
-		} catch (UnknownHostException e) {
-			System.out.println(e.getMessage());
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+            // Wait a bit
+            Thread.sleep(30000);
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
--- SmartOffice/src/main/java/jmdnsfiles/SmartOfficeServiceRegistration.java
+++ SmartOffice/src/main/java/jmdnsfiles/SmartOfficeServiceRegistration.java
@@ -11,10 +11,24 @@ public class SmartOfficeServiceRegistration {
 	public static void main(String[] args) throws InterruptedException {
 
         try {
+            // Create a JmDNS instance
             JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
 
-            ServiceInfo serviceInfo = ServiceInfo.create("_http._tcp.local.", "example", 8000, "path=index.html");
-            jmdns.registerService(serviceInfo);
+            // Register a service
+            ServiceInfo light = ServiceInfo.create("_http._tcp.local.", "light", 50051, "path=index.html");
+            jmdns.registerService(light);
+            ServiceInfo thermostat = ServiceInfo.create("_http._tcp.local.", "thermostat", 50052, "path=index.html");
+            jmdns.registerService(thermostat);
+            ServiceInfo printer = ServiceInfo.create("_http._tcp.local.", "printer", 50053, "path=index.html");
+            jmdns.registerService(printer);
+            ServiceInfo projector = ServiceInfo.create("_http._tcp.local.", "projector", 50054, "path=index.html");
+            jmdns.registerService(projector);
+
+            // Wait a bit
+            Thread.sleep(25000);
+
+            // Unregister all services
+            jmdns.unregisterAllServices();
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
